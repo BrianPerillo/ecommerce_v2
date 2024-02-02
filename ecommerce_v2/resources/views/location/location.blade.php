c2dc9af6dc2eaebe2b369644097a7fd2f4f2fd7b
BrianPerillo <brian_perillo@hotmail.com>
2024-02-02 15:52:54 -0300

    Agrego mapa incrustado - Google maps

--- ecommerce_v2/resources/views/location/location.blade.php
+++ ecommerce_v2/resources/views/location/location.blade.php
@@ -7,25 +7,18 @@
 
     <div class="py-12 bg-white">
 
-        <div class="container" style="overflow: hidden">
-            <h1>Tiendas</h1>
+
+        <div class="container">
+            <h1 style="text-align:center">Tiendas</h1>
+
+            <!--The div element for the map -->
+            <div style="height: 450px" id="map"></div>
+
         </div>
 
-        @if(isset($stores) && $stores !== null)
-            <div>
-                @foreach($stores as $store)
-                    <p>{{$store->country}}</p>  
-                    <p>{{$store->state}}</p>  
-                    <p>{{$store->city}}</p>  
-                    <p>{{$store->street}}</p>  
-                    <p>{{$store->streetnumber}}</p>  
-                @endforeach
-            </div>
-        @endif
+        <section class="container" style="margin-top:25px ">
 
-        <div class="col-lg-6 d-flex flex-column">
-            <h5 class="text-center">Buscar Farmacias Cercanas con Ubicación del navegador</h5>
-
+            <h5 class="text-center">Buscar Farmacias Cercanas</h5>
             <form id="search_stores_aut" action="#" class="my-5 row text-center flex-grow-1">
     
                 <div class="col-12 d-flex align-items-center justify-content-center">
@@ -33,12 +26,11 @@
                 </div>
     
             </form>
-        </div>
 
-        <section class="container">
             <div class="stores">
                   <!-- title -->
             </div>
+
             <div class="row">
                 <div id="results-grid" class="container mt-5 row">
                     <!-- resultados -->
@@ -46,7 +38,7 @@
             </div>
     
         </section>
-
+<!-- 
         <form id="search_stores_aut" action="{{ route('location.search') }}" class="my-5 row text-center flex-grow-1">
     
             <div class="col-12 d-flex align-items-center justify-content-center">
@@ -54,7 +46,7 @@
             </div>
 
         </form>
-
+-->
     </div>
 
 
@@ -63,7 +55,43 @@
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
     crossorigin="anonymous">
     </script>
+     <!--<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtey5MySbKjp_kcyYGg8WLy4d5PY5SEqU&callback=initMap"></script>-->
 
+    <script src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLEMAPS_API_KEY')}}"></script>
+    <script>
+       // Initialize and add the map
+        let map;
+
+        async function initMap() {
+            // The location of Uluru
+            const position = { lat: -25.344, lng: 131.031 };
+            // Request needed libraries.
+            //@ts-ignore
+            const { Map } = await google.maps.importLibrary("maps");
+            const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+            // The map, centered at Uluru
+            map = new Map(document.getElementById("map"), {
+                zoom: 4,
+                center: position,
+                mapId: "DEMO_MAP_ID",
+            });
+            var marker = new google.maps.Marker({
+                position: {lat: -34.397, lng: 150.644},
+                map: map
+            });
+            
+        }
+
+        initMap();
+
+   
+    </script>
+ 
+    <!-- prettier-ignore -->
+    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyAtey5MySbKjp_kcyYGg8WLy4d5PY5SEqU", v: "beta"});
+    </script>
 
     {{-- Relizar petición a la api --}}
     @include('location.locationRequest')
--- ecommerce_v2/resources/views/location/locationRequest.blade.php
+++ ecommerce_v2/resources/views/location/locationRequest.blade.php
@@ -63,7 +63,6 @@
                                         </div>
                                     </div>
                                 `;
-
                                 resultsGrid.append(card);
 
                         },
