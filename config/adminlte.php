1976afae222ccd97fda20474743989e877b9846c
BrianPerillo <brian_perillo@hotmail.com>
2024-02-09 21:15:44 -0300

    corrigo margen menu stock admin

--- config/adminlte.php
+++ config/adminlte.php
@@ -464,6 +464,7 @@ return [
                     'text' => 'Stock',
                     'url'  => '#',
                     'icon'  => 'fas  fa-edit fa-sm',
+                    'shift'   => 'ml-3',
                 ],
             ],
         ],
